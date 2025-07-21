a61b4c1d6879d8a0cf85b3ab980d8684e71a437a
Eric T <tchepane@hey.com>
2025-07-21 18:23:52 +0100

    Add no-args constructor to Keys

--- cashu-lib-common/src/main/java/xyz/tcheeric/cashu/common/Keys.java
+++ cashu-lib-common/src/main/java/xyz/tcheeric/cashu/common/Keys.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import xyz.tcheeric.cashu.common.json.deserializer.KeysDeserializer;
 import xyz.tcheeric.cashu.common.json.serializer.KeysSerializer;
 
@@ -15,6 +16,7 @@ import java.util.Map;
 @JsonDeserialize(using = KeysDeserializer.class)
 @JsonSerialize(using = KeysSerializer.class)
 @AllArgsConstructor
+@NoArgsConstructor
 public class Keys {
 
     private final Map<BigInteger, PublicKey> values = new HashMap<>();
